7ac8f805d9176ecb92a9bca303570be0fa250937
se-hyun hwang <imscs21@naver.com>
2014-03-20 17:33:10 +0900

    f

--- grboard/module/blog/admin/query.php
+++
@@ -1,6 +0,0 @@
-<?php
-$query = array(
-	'get_blog_info' => 'select * from ' . $db_prefix_blog . 'config limit 1' ,
-	'update_blog_config' => 'update ' . $db_prefix_blog . 'config set {0} limit 1'
-);
-?>
--- grboard/module/board/admin/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!defined('GR_BOARD_2')) exit();
-
-$skin = 'basic';
-$skinResourcePath = 'module/board/admin/skin/' . $skin;
-include 'skin/' . $skin . '/index.php';
-?>
